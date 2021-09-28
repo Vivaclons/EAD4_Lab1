19c95b54da21b854242f0c4bddd13a1726699791
mukhametkaly <60399541+mukhametkaly@users.noreply.github.com>
2021-09-28 12:26:50 +0600

    auth added

---
+++ auth/auth/src/main/java/com/example/demo/Models/User.java
@@ -0,0 +1,82 @@
+package com.example.demo.Models;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private List<Integer> basket;
+    
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = {
+                @JoinColumn(name = "user_id")
+        },
+        inverseJoinColumns = {
+                @JoinColumn(name = "role_id")
+        }
+    )
+    private List<Role> roles;
+
+    private boolean isAdmin;
+
+    public User(int id, String username, String password, List<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+
+        this.roles = roles;
+        isAdmin = false;
+        for (Role i  : roles ) {
+            if (i.getName().equals( "ADMIN" )) {
+                isAdmin = true;
+            }
+        }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
