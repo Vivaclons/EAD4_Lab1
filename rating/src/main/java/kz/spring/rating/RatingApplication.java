790e59423383827b830dd188045ddc5b5385132f
Vivaclons <s.erkyn00@gmail.com>
2021-10-24 21:26:06 +0600

    Added swagger-ui 2 and lombok

--- rating/src/main/java/kz/spring/rating/RatingApplication.java
+++ rating/src/main/java/kz/spring/rating/RatingApplication.java
@@ -7,9 +7,11 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class RatingApplication {
 
     public static void main(String[] args) {
