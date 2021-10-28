e86a5b3d67cfefd377d11b17cf294c6a2b8861d8
mukhametkaly <60399541+mukhametkaly@users.noreply.github.com>
2021-10-28 17:50:24 +0600

    hystrix dashboard added

--- order-api/src/main/java/com/example/demo/OrderApiApplication.java
+++ order-api/src/main/java/com/example/demo/OrderApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
-
+@EnableHystrixDashboard
 public class OrderApiApplication {
 
     public static void main(String[] args) {
